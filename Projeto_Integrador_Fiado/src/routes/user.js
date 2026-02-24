d3e22e8e6dee1bbf2bae57fa14e31e2fede869bf
CaponeCrD09 <felipesouzacrd09@gmail.com>
2026-02-24 20:01:43 -0300

    pastas user e seus doc

---
+++ Projeto_Integrador_Fiado/src/routes/user.js
@@ -0,0 +1,8 @@
+import {Router} from 'express';
+import { z } from 'zod';
+import { createUser } from '../services/user';
+
+const router = Router();
+// const zod = z();
+
+router.post('/', createUser);
