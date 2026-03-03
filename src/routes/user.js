8f21874ca2aecda8f6521907d1b331a275c4fb67
anthonydutra189 <anthonydutra189@gmail.com>
2026-03-03 19:37:35 -0300

    mudanças

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,15 @@
 import {Router} from 'express';
 import { z } from 'zod';
-import { createUser } from '../services/user.js';
+import { createUser, readPayment } from '../services/user.js';
 
 const router = Router();
 // const zod = z();
 
 router.post('/', createUser);
+
+
+
+router.get("/", readPayment)
+
 export default router;
 
