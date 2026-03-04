87639113b57dfd5be8f8ee580bc6b83b54790bf4
anthonydutra189 <anthonydutra189@gmail.com>
2026-03-03 21:03:27 -0300

    payment

--- src/services/payment.js
+++ src/services/payment.js
@@ -18,3 +18,9 @@ export async function readPayment(req, res, _next) {
     let users = await prisma.payment.findMany();
     return res.status(200).json(payment);
 }
+
+export async function showPayment(req, res, _next) {
+    let id = Number(req.params.id);
+    let u = await prisma.user.findFirst({where: {id:id}});
+    return res.status(200).json(payment);
+}
