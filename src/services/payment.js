63df208c4dad31dfa2b4d04702774040d9c01b9e
anthonydutra189 <anthonydutra189@gmail.com>
2026-03-03 22:29:48 -0300

    mudanças

--- src/services/payment.js
+++ src/services/payment.js
@@ -15,8 +15,24 @@ export async function createPayment(req, res, _next){
 }
 
 export async function readPayment(req, res, _next) {
-    let payment = await prisma.payment.findMany();
+   
+    const {value, credits_max, credits_min,user_id, payment_id,product_payment_id,company_id,payment_method }= req.query
+   
+    let consult= {}
+
+    if(value)  consult.value =  value
+    if(credits_max)  consult.value =  {lt: credits_max}
+    if(credits_min)  consult.value =  {gt: credits_min}
+    if(user_id) consult.userId = user_id
+    if(payment_id) consult.paymentId = payment_id
+    if(product_payment_id) consult.productPaymentId = product_payment_id
+    if(company_id) consult.paymentMethod = company_id
+    if(payment_method) consult.payment_method = 
+
+    let payment = await prisma.payment.findMany({where: consult});
+
     return res.status(200).json(payment);
+
 }
 
 export async function showPayment(req, res, _next) {
@@ -24,3 +40,4 @@ export async function showPayment(req, res, _next) {
     let p = await prisma.payment.findFirst({where: {id:id}});
     return res.status(200).json(p);
 }
+
