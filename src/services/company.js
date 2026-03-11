4b039f22ca72398fae92989ec904b7679aa678a8
wandersonneressouza <djandersonneres@hotmail.com>
2026-03-10 21:16:20 -0300

    vem nenem

--- src/services/company.js
+++ src/services/company.js
@@ -16,13 +16,25 @@ export async function createCompany(req, res, _next){
 }
 
 export async function readCompany(req, res, _next) {
-    let companies = await prisma.company.findMany();
-    return res.status(200).json(companies);
-}
+    const {name,category,place,cnpj,zipCode,addrres,phone,userId} = req.query;
+        let consult = {}
+
+        if(name) consult.name = {contains: "%" + name + "%"}
+        if(category) consult.category = {contains: "%" + category + "%"}
+        if(place) consult.place = {contains: "%" + place + "%"}
+        if(cnpj) consult.cnpj = {contains: "%" + cnpj + "%"}
+        if(zipCode) consult.zipCode = {contains: "%" + zipCode + "%"}
+        if(addrres) consult.addrres = {contains: "%" + addrres + "%"}
+        if(phone) consult.phone = {contains: "%" + phone + "%"}
+        if(userId) consult.userId = {contains: "%" + userId + "%"}
+
+        let companies = await prisma.company.findMany({where: consult});
+        return res.status(200).json(companies);
+    }
 
 export async function showCompany(req, res, _next) {
     let id = Number(req.params.id);
-    let companies = prisma.company.findFirst({where: {id:id}});
+    let companies = await prisma.company.findFirst({where: {id:id}});
     return res.status(200).json(companies);
 }
 
