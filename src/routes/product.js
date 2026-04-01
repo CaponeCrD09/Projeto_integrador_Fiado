66a4c23283ba833678760925931510c2906b566c
milenacarvalhu-jpg <milenacarvalhu@gmail.com>
2026-03-31 22:15:04 -0300

    criando uma regra de produtos

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,13 +1,14 @@
 import {Router} from 'express';
 import { z } from 'zod';
-import { createProducts,readProducts,showProducts,editProducts} from '../services/product.js';
+import { createProducts,readProducts,showProducts,editProducts,deleteProducts} from '../services/product.js';
 
 const router = Router();
 // const zod = z();
 
 router.post('/', createProducts);
-router.get('/',readProducts)
+router.get('/',readProducts);
 router.get('/:id',showProducts);
-router.put('/:id',editProducts)
+router.put('/:id',editProducts);
+router.delete('/:id',deleteProducts);
 
-export default router;
+export default router; 
