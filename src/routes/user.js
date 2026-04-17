c213294c112b21e7e4ef1cde1a45f93b0f473e11
anthonydutra189 <anthonydutra189@gmail.com>
2026-04-17 19:58:00 -0300

    meu

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,16 +2,16 @@ import { Router } from 'express';
 import { z } from 'zod';
 import { createUser, readUser, showUser, updateUser, deletando, loginUser } from '../services/user.js';
 
-import { authMiddleware } from "../middlewares/auth.js";
+//import { authMiddleware } from "../middlewares/auth.js";
 const router = Router();
 // const zod = z();
 
 router.post('/login', loginUser);
-router.post('/', /*authMiddleware,*/ createUser);
-router.get('/', /*authMiddleware,*/ readUser)
-router.get('/:id', /*authMiddleware,*/ showUser);
-router.put('/:id', /*authMiddleware,*/ updateUser);
-router.delete('/:id', /*authMiddleware,*/ deletando)
+router.post('/', createUser);
+router.get('/', readUser)
+router.get('/:id', showUser);
+router.put('/:id', updateUser);
+router.delete('/:id', deletando)
 
 
 
